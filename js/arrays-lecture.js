4dbf8b789e61f659998b2649ce6f069bed7003bf
Daniel Fryar <daniel.fryar@gmail.com>
2019-12-09 16:39:35 -0600

    finished with walkthrough and tictactoe game

--- js/arrays-lecture.js
+++ js/arrays-lecture.js
@@ -1,15 +1,30 @@
 var pets = ["Toby","Princess","Samson","Jill","Bubbles","Malu"];
 
 for (var i = 0;i<pets.length;i++) {
-    console.log(pets[i]);
+    // console.log(pets[i]);
 }
 
 pets[pets.length] = "Lily";
-console.log(pets[pets.length-1]);
+// console.log(pets[pets.length-1]);
 
 // pets[100] = "Roxeanne";
 pets.forEach(function (pet, i, arr) {
-    console.log(pet);
+    // console.log(pet);
     // console.log(arr[i]);
 })
 
+
+var tictactoe = [[' ', ' ', ' '],
+                 [' ', ' ', ' '],
+                 [' ', ' ', ' ']];
+var takeTurn = function(who, row, column) {
+    tictactoe[row][column] = who;
+}
+var printBoard = function() {
+    for (var i = 0;i<tictactoe.length;i++) {
+        console.log(' ' + tictactoe[i][0] + ' | ' + tictactoe[i][1] + ' | ' + tictactoe[i][2]);
+        console.log("___________")
+    }
+}
+
+printBoard();
